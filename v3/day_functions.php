7c09fccd5df70ee0ca0934116adb55be854c9988
Magnus Claesson <magnus.claesson@gmail.com>
2022-01-21 15:19:30 +0100

    refactoring

--- v3/day_functions.php
+++ v3/day_functions.php
@@ -1,171 +1,144 @@
 <?
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-function get_holiday($checkdate){
-$year = date("Y", $checkdate);
-$month = date("m", $checkdate);
-$day = date("d", $checkdate);
-    
-    
-    //Midsommar
-    if ($month == 6 && $day >= 19 && $day <= 25 && date("N",$checkdate) == 5){
-        return array("helgdag", "Midsommarafton", "Ja","Nej");
-    }
-    if ($month == 6 && $day >= 20 && $day <= 26 && date("N",$checkdate) == 6){
-        return array("helgdag", "Midsommardagen", "Ja","Ja");
-    }
 
-    //Påsk
-    if (date("Ymd",strtotime('-3 day', easter($year))) == date("Ymd",$checkdate)){
-       return array("helgdagsafton", "Skärtorsdagen", "Nej","Nej"); 
+function get_holiday ($checkdate) {
+  $year = date('Y', $checkdate);
+  $month = date('n', $checkdate);
+  $day = date('j', $checkdate);
+  
+  //Midsommar
+  if ($month === 6 && $day >= 19 && $day <= 25 && date('N', $checkdate) === 5) {
+    return array('helgdag', 'Midsommarafton', true, false);
+  }
+  if ($month === 6 && $day >= 20 && $day <= 26 && date('N', $checkdate) === 6) {
+    return array('helgdag', 'Midsommardagen', true, true);
+  }
+  
+  //Påsk
+  if (date('Ymd', strtotime('-3 day', easter($year))) === date('Ymd', $checkdate)) {
+    return array('helgdagsafton', 'Skärtorsdagen', false, false); 
+  }
+  
+  if (date('Ymd', strtotime('-2 day', easter($year))) === date('Ymd', $checkdate)) {
+    return array('helgdag', 'Långfredagen', true, true);
+  }
+  
+  if (date('Ymd', strtotime('-1 day', easter($year))) === date('Ymd', $checkdate)) {
+    return array('helgdag', 'Påskafton', true, false); 
+  }
+  
+  if (date('Ymd', easter($year)) === date('Ymd', $checkdate)) {
+    return array('helgdag', 'Påskdagen', true, true); 
+  }
+  
+  if (date('Ymd', strtotime('+1 day', easter($year))) === date('Ymd', $checkdate)) {
+    return array('helgdag', 'Annandag påsk', true, true); 
+  }
+  
+  if (date('Ymd', strtotime('+39 day', easter($year))) === date('Ymd', $checkdate)) {
+    if ($month == 4 && $day === 30) {
+      return array('helgdag', 'Kristi himmelsfärdsdag, Valborgsmässoafton', true, true);
+    } elseif ($month === 5 && $day === 1) {
+      return array('helgdag', 'Kristi himmelsfärdsdag, Första Maj', true, true);
+    } else {
+      return array('helgdag', 'Kristi himmelsfärdsdag', true, true);    
     }
-    
-    if (date("Ymd",strtotime('-2 day',easter($year))) == date("Ymd",$checkdate)){
-       return array("helgdag", "Långfredagen", "Ja","Ja");
+  }
+  
+  if (date('Ymd',strtotime('+48 day',easter($year))) === date('Ymd', $checkdate)) {
+    return array('helgdagsafton', 'Pingstafton', true, false); 
+  }
+  
+  if (date('Ymd',strtotime('+49 day',easter($year))) === date('Ymd', $checkdate)) {
+    return array('helgdag', 'Pingstdagen', true, true); 
+  }
+  
+  if (date('Ymd',strtotime('+50 day',easter($year))) === date('Ymd', $checkdate)) {
+    if ($year < 2005) {
+      return array('helgdag', 'Annandag pingst', true, true); 
+    } else {
+      return array('helgdag', 'Annandag pingst', false, false); 
     }
+  }
+  
+  //Allhelgona
+  if ($checkdate >= strtotime($year . '-10-30') && $checkdate <= strtotime($year . '-11-05') && date('N', $checkdate) === 5) {
+    return array('helgdagsafton', 'Allhelgonaafton', false, false);    
+  }
+  
+  if ($checkdate >= strtotime($year . '-10-31') && $checkdate <= strtotime($year . '-11-06') && date('N', $checkdate) === 6) {
+    return array('helgdag', 'Alla helgons dag', true, true);    
+  } 
 
-    if (date("Ymd",strtotime('-1 day',easter($year))) == date("Ymd",$checkdate)){
-       return array("helgdag", "Påskafton", "Ja","Nej"); 
-    }
+  //Fixed dates Returnerar typ, dag och om den är abetsfri eller inte OCH om det är röd dag
+  $fixed['01']['01'] = array('helgdag', 'Nyårsdagen', true, true);
+  $fixed['01']['05'] = array('helgdagsafton', 'Trettondagsafton', false, false);
+  $fixed['01']['06'] = array('helgdag', 'Trettondedag jul', true, true);
+  $fixed['04']['30'] = array('helgdagsafton', 'Valborgsmässoafton', false, false);
+  $fixed['05']['01'] = array('helgdag', 'Första Maj', true, true);
+  $fixed['06']['06'] = array('helgdag', 'Sveriges nationaldag', true, true);
+  $fixed['12']['24'] = array('helgdag', 'Julafton', true, false);
+  $fixed['12']['25'] = array('helgdag', 'Juldagen', true, true);
+  $fixed['12']['26'] = array('helgdag', 'Annandag jul', true, true);
+  $fixed['12']['31'] = array('helgdag', 'Nyårsafton', true, false);
 
-    if (date("Ymd",easter($year)) == date("Ymd",$checkdate)){
-       return array("helgdag", "Påskdagen", "Ja","Ja"); 
-    }
-    
-    if (date("Ymd",strtotime('+1 day',easter($year))) == date("Ymd",$checkdate)){
-       return array("helgdag", "Annandag påsk", "Ja","Ja"); 
-    }
-    
-    if (date("Ymd",strtotime('+39 day',easter($year))) == date("Ymd",$checkdate)){
-       if ($month == "04" && $day == "30"){
-           return array("helgdag", "Kristi himmelsfärdsdag, Valborgsmässoafton", "Ja","Ja");
-       }
-       
-       elseif($month == "05" && $day == "01"){
-           return array("helgdag", "Kristi himmelsfärdsdag, Första Maj", "Ja","Ja");
-       }
-       
-       else{
-            return array("helgdag", "Kristi himmelsfärdsdag", "Ja","Ja");    
-       }
-        
-    }
-    
-    if (date("Ymd",strtotime('+48 day',easter($year))) == date("Ymd",$checkdate)){
-       return array("helgdagsafton", "Pingstafton", "Ja","Nej"); 
-    }
-    
-    if (date("Ymd",strtotime('+49 day',easter($year))) == date("Ymd",$checkdate)){
-       return array("helgdag", "Pingstdagen", "Ja","Ja"); 
-    }
-    
-    if (date("Ymd",strtotime('+50 day',easter($year))) == date("Ymd",$checkdate))
-    {
-     if ($year < "2005")
-        {
-            return array("helgdag", "Annandag pingst", "Ja","Ja"); 
-        }
-    else
-        {
-       return array("helgdag", "Annandag pingst", "Nej","Nej"); 
-        }
-    }
-    
-    
-    //Allhelgona
-    if ($checkdate >= strtotime($year."-10-30") && $checkdate <= strtotime($year."-11-05") && date("N",$checkdate) == 5){
-        return array("helgdagsafton", "Allhelgonaafton", "Nej","Nej");    
-    }
-    
-    if ($checkdate >= strtotime($year."-10-31") && $checkdate <= strtotime($year."-11-06") && date("N",$checkdate) == 6){
-        return array("helgdag", "Alla helgons dag", "Ja","Ja");    
-    } 
+  //Ovveride for nationaldagen
+  if ($year < 2005) {
+    $fixed['06']['06'] = array('helgdag', 'Sveriges nationaldag', false, false);
+  }
+  
+  if ($year < 1983) {
+    $fixed['06']['06'] = array('helgdag', 'Svenska flaggans dag', false, false);
+  }
 
-
-
-    //Fixed dates Returnerar typ, dag och om den är abetsfri eller inte OCH om det är röd dag
-    $fixed["01"]["01"] = array("helgdag","Nyårsdagen","Ja","Ja");
-    $fixed["01"]["05"] = array("helgdagsafton","Trettondagsafton","Nej","Nej");
-    $fixed["01"]["06"] = array("helgdag","Trettondedag jul","Ja","Ja");
-    $fixed["04"]["30"] = array("helgdagsafton","Valborgsmässoafton","Nej","Nej");
-    $fixed["05"]["01"] = array("helgdag","Första Maj","Ja","Ja");
-    $fixed["06"]["06"] = array("helgdag","Sveriges nationaldag","Ja","Ja");
-    $fixed["12"]["24"] = array("helgdag","Julafton","Ja","Nej");
-    $fixed["12"]["25"] = array("helgdag","Juldagen", "Ja","Ja");
-    $fixed["12"]["26"] = array("helgdag","Annandag jul","Ja","Ja");
-    $fixed["12"]["31"] = array("helgdag","Nyårsafton","Ja","Nej");
-    
-    
-    
-    //Ovveride for nationaldagen
-    if ($year < "2005"){
-        $fixed["06"]["06"] = array("helgdag","Sveriges nationaldag","Nej","Nej");
-    }
-    
-    if ($year < "1983"){
-        $fixed["06"]["06"] = array("helgdag","Svenska flaggans dag","Nej","Nej");
-    }
-    
-    
-    
-    if (isset($fixed[$month][$day])){
-        return $fixed[$month][$day];
-    }
-
- 
-    
+  if (isset($fixed[$month][$day])) {
+    return $fixed[$month][$day];
+  }
 } //Slut get_holiday
-
 
 //Konvertering till riktiga påskdatum även innan 1970
 function easter($easteryear) {
-    $base = strtotime($easteryear."-03-21");
-    $days = easter_days($easteryear);
-    $easter = strtotime("+".$days." day", $base);
-    return $easter;
+  $base = strtotime($easteryear . '-03-21');
+  $days = easter_days($easteryear);
+  $easter = strtotime('+' . $days . ' day', $base);
+  return $easter;
 }
 
-
-    //Veckodagar (returnerar veckodag, om dagen är arbetsfri och om det är en röd dag)
-    function get_weekday($checkdate){
-        switch (date("N",$checkdate)) {
-            case 1:
-                return array("Måndag", "Nej", "Nej"); 
-                break;
-            case 2:
-                return array("Tisdag", "Nej", "Nej");
-                break;
-            case 3:
-                return array("Onsdag", "Nej", "Nej");
-                break;
-            case 4:
-                return array("Torsdag", "Nej", "Nej");
-                break;
-            case 5:
-                return array("Fredag", "Nej", "Nej");
-                break;
-            case 6:
-                return array("Lördag", "Ja", "Nej");
-                break;
-            case 7:
-                return array("Söndag", "Ja", "Ja");
-                break;
-        }   
-    }
-    //Slut veckodagar
-
-function is_workfree($checkdate, $offset){
-    $weekday = get_weekday(strtotime($offset,$checkdate));
-    $holiday = get_holiday(strtotime($offset,$checkdate));
-    
-    
-    
-    if ($weekday[1] == "Ja"  || $holiday[2] == "Ja"){
-        return true;
-    }
-    
-    
+//Veckodagar (returnerar veckodag, om dagen är arbetsfri och om det är en röd dag)
+function get_weekday($checkdate) {
+  switch (date('N', $checkdate)) {
+    case 1:
+      return array('Måndag', false, false); 
+      break;
+    case 2:
+      return array('Tisdag', false, false);
+      break;
+    case 3:
+      return array('Onsdag', false, false);
+      break;
+    case 4:
+      return array('Torsdag', false, false);
+      break;
+    case 5:
+      return array('Fredag', false, false);
+      break;
+    case 6:
+      return array('Lördag', true, false);
+      break;
+    case 7:
+      return array('Söndag', true, true);
+      break;
+  }
 }
+//Slut veckodagar
 
-
+function is_workfree($checkdate, $offset) {
+  $weekday = get_weekday(strtotime($offset, $checkdate));
+  $holiday = get_holiday(strtotime($offset, $checkdate));
+  
+  if ($weekday[1] === true || $holiday[2] === true) {
+    return true;
+  }
+}
 ?>
--- v3/index.php
+++ v3/index.php
@@ -6,92 +6,74 @@ require('day_functions.php');
 require('namedays.php');
 
 //Set up defaults
-$startyear=date("Y");
-$endyear=date("Y");
-$startmonth="01";
-$endmonth="12";
-$startday="01";
-$endday = "";
+$startyear = date('Y');
+$endyear = date('Y');
+$startmonth = '01';
+$endmonth = '12';
+$startday = '01';
+$endday = '';
+$params = '';
 
+if (strpos($_SERVER['REQUEST_URI'], '?') !== false) {
+  $uri_and_params = explode('?', $_SERVER['REQUEST_URI']);
+  $uri = $uri_and_params[0];
+  $params = $uri_and_params[1];
 
-if (strpos($_SERVER['REQUEST_URI'],'?') !== false) {
-$uri_and_params = explode('?', $_SERVER['REQUEST_URI']);
-$uri = $uri_and_params[0];
-$params = $uri_and_params[1];
+  $params = preg_replace('/[&]?(statistik)[&]?/', '', $params);
+} else {
+  $uri = $_SERVER['REQUEST_URI'];
 }
-else{
-    $uri = $_SERVER['REQUEST_URI'];
-}
-
-$request_parts = explode('/', $uri);
-
-
+$request_parts = explode('/', $params);
 
 //Check year
-if (!isset($request_parts[3]) || is_null($request_parts[3]) || $request_parts[3]==""){
-    $startyear=date("Y");
-    $startmonth=date("m");
-    $startday=date("d");
-    $endyear=date("Y");
-    $endmonth=date("m");
-    $endday=date("d");
-    }
-
-elseif (is_numeric($request_parts[3]) && $request_parts[3]>1901 && $request_parts[3]<3000)  {
-$startyear = $request_parts[3];
-$endyear = $startyear;
+if (!isset($request_parts[0]) || is_null($request_parts[0]) || $request_parts[0] === '') {
+  $startyear=date('Y');
+  $startmonth=date('m');
+  $startday=date('d');
+  $endyear=date('Y');
+  $endmonth=date('m');
+  $endday=date('d');
+} elseif (is_numeric($request_parts[0]) && $request_parts[0] > 1901 && $request_parts[0] < 3000) {
+  $startyear = $request_parts[0];
+  $endyear = $startyear;
+} else {
+  return_error('Felaktigt årtal');
 }
-
-else{
-return_error("Felaktigt årtal");
-}
-
-
-
 
 //Check month
-
-if (!isset($request_parts[4]) || is_null($request_parts[4]) || $request_parts[4]==""){
-//Use defaults
-}
-
-elseif (is_numeric($request_parts[4]) && $request_parts[4]>0 && $request_parts[4]<13)  {
-$startmonth = $request_parts[4];
-$endmonth = $request_parts[4];
-}
-else{
-return_error("Felaktig månad");
+if (!isset($request_parts[1]) || is_null($request_parts[1]) || $request_parts[1] === '') {
+  //Use defaults
+} elseif (is_numeric($request_parts[1]) && $request_parts[1] > 0 && $request_parts[1] < 13) {
+  $startmonth = $request_parts[1];
+  $endmonth = $request_parts[1];
+} else {
+  return_error('Felaktig månad');
 }
 
 //check day
-if ($endday != ""){
-    //Use set values
-}
-
-elseif (!isset($request_parts[5]) || is_null($request_parts[5]) || $request_parts[5]==""){
-    $startday="01";
-    $endday=cal_days_in_month(CAL_GREGORIAN, $startmonth, $startyear);
-}
-
-elseif (is_numeric($request_parts[5]) && $request_parts[5]>0 && $request_parts[5]<=cal_days_in_month(CAL_GREGORIAN, $startmonth, $startyear))  {
-$startday = $request_parts[5];
-$endday = $request_parts[5];
-}
-else{
-return_error("Felaktig dag ".$request_parts[5]);
+if ($endday !== '') {
+  //Use set values
+} elseif (!isset($request_parts[2]) || is_null($request_parts[2]) || $request_parts[2] === '') {
+  $startday = '01';
+  $endday = cal_days_in_month(CAL_GREGORIAN, $startmonth, $startyear);
+} elseif (is_numeric($request_parts[2]) && $request_parts[2] > 0 && $request_parts[2] <= cal_days_in_month(CAL_GREGORIAN, $startmonth, $startyear)) {
+  $startday = $request_parts[2];
+  $endday = $request_parts[2];
+} else {
+  return_error('Felaktig dag '.$request_parts[2]);
 }
 
 //Format the dates
-$startunixdate = mktime(0,0,0,$startmonth,$startday,$startyear);
-$endunixdate = mktime(0,0,0,$endmonth,$endday,$endyear);
+$startunixdate = mktime(0, 0, 0, $startmonth, $startday, $startyear);
+$endunixdate = mktime(0, 0, 0, $endmonth, $endday, $endyear);
 
-
-$output['cachetid'] = date("Y-m-d H:m:s");
-$output['version'] = "2.0";
+$output['_timestamp'] = time();
+$output['api version'] = 'v3.1.0';
 $output['uri'] = $_SERVER['REQUEST_URI'];
-$output['startdatum'] = date("Y-m-d", $startunixdate);
-$output['slutdatum'] = date("Y-m-d", $endunixdate);
-
+$output['startdatum'] = date('Y-m-d', $startunixdate);
+$output['slutdatum'] = date('Y-m-d', $endunixdate);
+$output['contributors']['developers'] = ['https://dryg.net/', 'https://github.com/Lavve/'];
+$output['contributors']['namnsdagar'] = 'https://dagensnamnsdag.com/';
 
 //Time to loop it!
 $number_of_days = 0;
@@ -102,82 +84,88 @@ $squeeze_days['totalt antal'] = 0;
 
 $loopdate = $startunixdate;
 
-    while ($loopdate <= $endunixdate) {
-    	$output['dagar'][$number_of_days]['datum'] = date("Y-m-d", $loopdate);
-        
-        //check weekday
-        list($weekday, $workfree, $redday) = get_weekday($loopdate);
-        $output['dagar'][$number_of_days]['veckodag'] = $weekday;
-        $output['dagar'][$number_of_days]['arbetsfri dag'] = $workfree;
-        $output['dagar'][$number_of_days]['röd dag'] = $redday;
-        
-        //Check if day is holiday
-        if ($type_and_day = get_holiday($loopdate)){
-            list($type, $day, $workfree_holiday, $redday_holiday) = $type_and_day;
-            $output['dagar'][$number_of_days][$type] = $day;
-            
-            if ($workfree == "Ja" || $workfree_holiday == "Ja"){
-              $workfree = "Ja";  
-            }
-            
-            if ($redday == "Ja" || $redday_holiday == "Ja"){
-            $redday = "Ja";
-            }
-            
-            $output['dagar'][$number_of_days]['arbetsfri dag'] = $workfree;
-            $output['dagar'][$number_of_days]['röd dag'] = $redday;
-            
-        }
-        
-        
-    if ($workfree == "Ja"){
-        $number_of_workfree++;
-    }
-    else {
-       $number_of_work++;
-       $next_day = get_holiday(strtotime('+1 day',$loopdate));
-       if ($next_day[0] == "helgdag"){
-           $output['dagar'][$number_of_days]['dag före arbetsfri helgdag'] = "Ja";
-       }
-       
-       if (is_workfree($loopdate,"+1 day") == true && is_workfree($loopdate,"-1 day") == true){
-           $output['dagar'][$number_of_days]['klämdag'] = "Ja";
-           $squeeze_days['totalt antal']++;
-           $squeeze_days[] = date("Y-m-d", $loopdate);
-       }
-       
+while ($loopdate <= $endunixdate) {
+  $output['dagar'][$number_of_days]['datum'] = date('Y-m-d', $loopdate);
+  
+  //check weekday
+  list($weekday, $workfree, $redday) = get_weekday($loopdate);
+  $output['dagar'][$number_of_days]['veckodag'] = $weekday;
+  $output['dagar'][$number_of_days]['arbetsfri dag'] = $workfree;
+  $output['dagar'][$number_of_days]['röd dag'] = $redday;
+  
+  //Check if day is holiday
+  if ($type_and_day = get_holiday($loopdate)){
+    list($type, $day, $workfree_holiday, $redday_holiday) = $type_and_day;
+    $output['dagar'][$number_of_days][$type] = $day;
+    
+    if ($workfree === true || $workfree_holiday === true){
+      $workfree = true;  
     }
     
-    //Get namedays
-    if($names = get_nameday($loopdate)){
-        $output['dagar'][$number_of_days]['namnsdag'] = $names;
+    if ($redday === true || $redday_holiday === true){
+      $redday = true;
     }
     
-    //Keep this last in the loop please!
-	$loopdate = strtotime('+1 day', $loopdate);
-    $number_of_days++;
-	}
+    $output['dagar'][$number_of_days]['arbetsfri dag'] = $workfree;
+    $output['dagar'][$number_of_days]['röd dag'] = $redday;
+  }
+  
+  if ($workfree === true) {
+    $number_of_workfree++;
+  } else {
+    $number_of_work++;
+    $next_day = get_holiday(strtotime('+1 day',$loopdate));
+
+    if ($next_day[0] === 'helgdag'){
+      $output['dagar'][$number_of_days]['dag före arbetsfri helgdag'] = true;
+    }
+    
+    if (is_workfree($loopdate,'+1 day') && is_workfree($loopdate,'-1 day')){
+      $output['dagar'][$number_of_days]['klämdag'] = true;
+      $squeeze_days['totalt antal']++;
+      $squeeze_days[] = date('Y-m-d', $loopdate);
+    }
+  }
+  
+  //Get namedays
+  if ($names = get_nameday($loopdate)){
+    $output['dagar'][$number_of_days]['namnsdag'] = $names;
+  }
+  
+  //Keep this last in the loop please!
+  $loopdate = strtotime('+1 day', $loopdate);
+  $number_of_days++;
+}
+
+function count_names ($dagar) {
+  $number_of_names = 0;
+  foreach($dagar as $day) {
+    if (array_key_exists('namnsdag', $day)) {
+      $number_of_names += count($day['namnsdag']);
+    }
+  }
+
+  return $number_of_names;
+}
 
 //Store statistics
 if (isset($_GET['statistik'])){
-$output['statistik']['antal dagar'] = $number_of_days;
-$output['statistik']['arbetsfria dagar'] = $number_of_workfree;
-$output['statistik']['arbetsdagar'] = $number_of_work;
-$output['statistik']['klämdagar'] = $squeeze_days;
+  $output['statistik']['antal dagar'] = $number_of_days;
+  $output['statistik']['arbetsfria dagar'] = $number_of_workfree;
+  $output['statistik']['arbetsdagar'] = $number_of_work;
+  $output['statistik']['klämdagar'] = $squeeze_days;
+  $output['statistik']['antal namnsdagar'] = count_names($output['dagar']);
 }
 
 //Push it out
-header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json; charset=utf-8');
 $json = json_encode($output);
 $jsonp_callback = isset($_GET['callback']) ? $_GET['callback'] : null;
-print $jsonp_callback ? "$jsonp_callback($json)" : $json;
-
-
-
+print $jsonp_callback ? '$jsonp_callback($json)' : $json;
 
 function return_error($msg){
-    header('Status: 400 Bad Request', true, 400);
-    exit($msg);
-    }
-
+  header('Status: 400 Bad Request', true, 400);
+  exit($msg);
+}
 ?>
